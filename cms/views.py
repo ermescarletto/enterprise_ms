5cb4ed4dc14a0d7d141e108ece62ae341bc834fe
ermescarletto <ermes.carletto@gmail.com>
2025-01-30 10:39:01 -0300

    Criacao de Templates de Gerenciamento de Usuários

--- cms/views.py
+++ cms/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
 from django.shortcuts import redirect
 
-
-class Index(TemplateView):
-
+class Index(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
-
-
+    login_url = 'auth/login/'  # Defina a URL para onde os usuários não autenticados serão redirecionados
--- enterprise/settings.py
+++ enterprise/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = 'django-insecure-jkcrj2(q%41y8_h5-g=8rl990g_*7bj+ao^0lh#@zy)wvnil2e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+LOGIN_URL = 'auth/login/'
+
 ALLOWED_HOSTS = []
 
 
