342fd616da0b93b0397171f617c700fea90c3416
Carletto Jr. <ermes.carletto@gmail.com>
2025-04-08 00:18:23 -0300

    celery

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     ### UNIDADE E GERENTE, TESTANDO O TIPO DE INCLUSÃO DA ROTA
 
     path('api/cidades/', CidadeList.as_view(), name='api-cidade-list'),
-    path('api/cidades/detail/', CidadeCRUD.as_view(), name='api-cidade-crud'),
+    path('api/cidades/<int:id>/', CidadeCRUD.as_view(), name='api-cidade-crud'),
     path('api/pessoasfisicas/', PessoaFisicaList.as_view(), name='pessoas_fisicas'),
     path('api/pessoasfisicas/<int:id>', PessoaFisicaList.as_view(), name='pessoas-fisicas-crud'),
 #API PARA PJ
     path('api/pessoajuridica/', PessoaJuridicaListCreate.as_view(), name='pessoas-juridicas'),
     path('api/pessoajuridica/<int:id>', PessoaJuridicaCRUD.as_view(), name='pessoas-fisicas-crud'),
-#API PARA UNIDADE
+#API PARA UNIDADE0
     path('api/unidade/', UnidadeListCreate.as_view(), name='pessoas-juridicas'),
     path('api/unidade/<int:id>', UnidadesCRUD.as_view(), name='pessoas-fisicas-crud'),
 
